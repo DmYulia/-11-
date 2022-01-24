7b183c31835268d9273ac277479a14a74f58821e
Юлия <@LAPTOP-B0K7S1UE>
2022-01-24 20:23:23 +0700

    Добавьте файлы проекта.

---
+++ 11 структуры/LineUr.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _11_структуры
+{
+    struct LineUr
+    {
+        private readonly double k;
+        private readonly double b;
+        private string answer;
+        public LineUr(double k, double b)
+        {
+            this.k = k;
+            this.b = b;
+            answer = "";
+        }
+        public void Root()
+        {
+            if (k == 0)
+            {
+                if (b == 0)
+                {
+                    Console.WriteLine("Уравнение с заданными параметрами имеет бесконечное количество решений.");
+                }
+                else
+                {
+                    Console.WriteLine("Уравнение с заданными параметрами не имеет решений в вещетвенных числах.");
+                }
+            }
+            else
+            {
+                answer = string.Format("{0:f6}", (-b / k));
+                Console.WriteLine($"Решение уравнения 0 = ({k})x + ({b}) найдено. x = {answer} ");
+            }
+        }
+    }
+}
---
+++ 11 структуры/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace _11_структуры
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double k = 0, b = 0;
+            WriteLine("Введите коэффициенты уравнения вида 0 = kx + b :");
+            BK:
+            try
+            {
+            Write("k = ");
+            k = Convert.ToDouble(ReadLine());
+            }
+            catch (Exception e)
+            {
+                WriteLine(e.Message);
+                goto BK;
+            }
+            BB:
+            try
+            {
+                Write("b = ");
+                b = Convert.ToDouble(ReadLine());
+            }
+            catch (Exception e)
+            {
+                WriteLine(e.Message);
+                goto BB;
+            }
+            LineUr answ = new LineUr(k, b);
+            answ.Root();
+            ReadKey();
+        }
+    }
+}
